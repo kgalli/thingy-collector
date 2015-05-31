b3e3570b2ef031bed0c6a67a60c9133d9940e6eb
kgalli <mail@kgalli.de>
2015-05-31 11:57:37 +0000

    Initial commit

---
+++ lib/thingy/collector.rb
@@ -0,0 +1,7 @@
+require "thingy/collector/version"
+
+module Thingy
+  module Collector
+    # Your code goes here...
+  end
+end
---
+++ lib/thingy/collector/version.rb
@@ -0,0 +1,5 @@
+module Thingy
+  module Collector
+    VERSION = "0.1.0"
+  end
+end
