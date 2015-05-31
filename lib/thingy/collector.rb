47870f8b1d4cc2df04c2ee10d73635d4d924c32e
kgalli <mail@kgalli.de>
2015-05-31 15:26:07 +0000

    Add dynamic attributes incl. accessors

--- lib/thingy/collector.rb
+++ lib/thingy/collector.rb
@@ -2,6 +2,32 @@ require "thingy/collector/version"
 
 module Thingy
   module Collector
-    # Your code goes here...
+
+    def self.included(base)
+      base.class_eval do
+        extend ClassMethods
+      end
+    end
+
+    module ClassMethods
+      def attributes(*attrs)
+        if attrs.any?
+          self.attributes.merge(attrs)
+          attrs.each do |attr|
+            define_attr_accessor(attr) unless attribute_defined?(attr)
+          end
+        else
+          @attributes ||= Set.new
+        end
+      end
+
+      def define_attr_accessor(attr)
+        attr_accessor(attr)
+      end
+
+      def attribute_defined?(name)
+        instance_methods.include?(name)
+      end
+    end
   end
 end
--- spec/thingy/collector_spec.rb
+++ spec/thingy/collector_spec.rb
@@ -5,7 +5,32 @@ describe Thingy::Collector do
     expect(Thingy::Collector::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  before(:each) do
+    class Book
+      include Thingy::Collector
+      attributes :title, :author
+    end
+  end
+
+  describe '.attributes' do
+    it "defines attributes" do
+      expect(Book.attributes).to eql Set.new([:title, :author])
+    end
+  end
+
+  describe 'accessors' do
+    it 'provides getters for attributes' do
+      book = Book.new
+      book.title = 'The Story'
+      expect(book.title).to eql 'The Story'
+      expect(book.author).to eql nil
+    end
+
+    it 'provide setter for attributes' do
+      book = Book.new
+      book.title = 'The Story'
+      expect(book.instance_variable_get(:@title)).to eql 'The Story'
+      expect(book.title).to eql 'The Story'
+    end
   end
 end
